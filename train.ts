a0b93c1d8000d9a32a9674778df62cabcd681e2a
Patrick Weingärtner <patrick.weingaertner@gmail.com>
2025-11-15 12:04:33 +0100

    fix train url and new classes

--- train.ts
+++ train.ts
@@ -4,7 +4,7 @@ import { JSDOM } from 'jsdom'
 
 export async function getTimetableChanges() {
 	try {
-		const res = await axios.get('https://sbahn.berlin/fahren/bauen-stoerung/?tabs=tbc-l7')
+		const res = await axios.get('https://sbahn.berlin/fahren/bauen-stoerung/?filterByLine=S2&timeframetab=3')
 		const dom = new JSDOM(res.data, { runScripts: 'dangerously' })
         	const document = dom.window.document
 
@@ -14,25 +14,25 @@ export async function getTimetableChanges() {
 
 	        // on these elements map following
 	        //
-        	// line numbers
-	        // x.getElementsByClassName('c-construction-announcement-head')[0].children[0].textContent
+        	// line number
+	        // x.getElementsByClassName('c-line-badge-list')[0].children[0]?.textContent
         	//
 	        // station info
-        	// x.getElementsByClassName('o-construction-announcement-title')[0].children[0].textContent
+        	// x.getElementsByClassName('o-heading:4')[0]?.textContent
 	        //
 			// date info
 			// Array.from(x.getElementsByClassName('o-timespan__cp')).map(t => t.textContent).toString()
 			// 
 	        // closer information
-        	// x.getElementsByClassName('c-list-unordered')[0].children[0].textContent
+        	// x.getElementsByClassName('c-con-detail__effect')[0].textContent
 	        //
-        
-		return Array.from(document.getElementsByClassName("c-constructions")[2].children).filter((x: any) => x.attributes.getNamedItem('data-lines')?.nodeValue.match(/\bs2\b/g))
+
+		return Array.from(document.getElementsByClassName("c-box-list__item"))
 	        .map((x: Element) => [
-        	        x.getElementsByClassName('c-construction-announcement-head')[0].children[0].textContent,
-                	x.getElementsByClassName('o-construction-announcement-title')[0].children[0].textContent,
-					Array.from(x.getElementsByClassName('o-timespan__cp')).map(t => t.textContent).toString(),
-	                x.getElementsByClassName('c-list-unordered')[0].children[0].textContent
+        	        x.getElementsByClassName('c-line-badge-list')[0].children[0]?.textContent,
+                	x.getElementsByClassName('o-heading:4')[0]?.textContent,
+					Array.from(x.getElementsByClassName('c-box__period')).map(t => t.textContent).toString(),
+	                x.getElementsByClassName('c-con-detail__effect')[0]?.textContent
        	 	])
        
 	} catch (e) {
